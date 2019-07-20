1d0bbc364bd2e841f80ab5abdbc17e16b8429619
DarthCorvidus <github@vm01.telton.de>
2019-07-20 16:00:18 +0200

    Interace for conversion classes as well as ConvertDate to convert dates between different formats.

---
+++ include/lib/Typetools/Convert/Convert.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Claus-Christoph Kuethe 2007
+ * @license http://www.gnu.org/licenses/lgpl.html
+ * @author Claus-Christoph Küthe <lnkbackup@vm01.telton.de>
+ */
+
+/**
+ * Convert
+ * 
+ * Interface to define small converter classes.
+ */
+interface Convert {
+	function convert($convertee);
+}
+?>
