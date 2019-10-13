31dbe46a9d3d7806557a75f2feb96f90c6e4a0a7
DarthCorvidus <github@vm01.telton.de>
2019-10-13 16:41:06 +0200

    Refactored ArgString, made ArgDate an extended class of ArgString, added ArgFile to validate for directories and files.

--- include/lib/Argv/ArgDate.php
+++ include/lib/Argv/ArgDate.php
@@ -4,52 +4,9 @@
  * @author Claus-Christoph Küthe <floss@vm01.telton.de>
  * @license LGPL
  */
-class ArgDate implements ArgModel {
-	private $default;
-	private $name;
-	private $mandatory = false;
-	public function __construct(string $name, string $default = "") {
-		$this->name = $name;
-		$this->default = $default;
-	}
-	
-	public function setMandatory(bool $mandatory) {
-		$this->mandatory = $mandatory;
-	}
-	
-	public function getConvert(): Convert {
-		
-	}
-
-	public function getDefault(): string {
-		return $this->default;
-	}
-
-	public function getLongName(): string {
-		return $this->name;
-	}
-
-	public function isMandatory(): bool {
-		return $this->mandatory;
-	}
-
-	public function getShortName(): string {
-		return "";
-	}
-
-	public function getValidate(): \Validate {
-		return new ValidateDate(ValidateDate::ISO);
-	}
-
-	public function hasConvert(): bool {
-		return false;
-	}
-
-	public function hasDefault(): bool {
-		return $this->default!=="";
-	}
-
-	public function hasValidate(): bool {
-		return true;
+class ArgDate extends ArgString {
+	public function __construct() {
+		parent::__construct();
+		$this->setValidate(new ValidateDate(ValidateDate::ISO));
 	}
 }
--- include/lib/Argv/ArgString.php
+++ include/lib/Argv/ArgString.php
@@ -5,58 +5,51 @@
  * @license LGPL
  */
 class ArgString implements ArgModel {
-	private $name;
 	private $default;
 	private $mandatory = false;
 	private $validate;
 	private $convert;
-	public function __construct(string $name, string $default = "") {
-		$this->name = $name;
-		$this->default = $default;
+	public function __construct() {
 	}
 
 	public function setMandatory(bool $mandatory) {
 		$this->mandatory = $mandatory;
 	}
 	
-	public function setValidate(Validate $validate) {
-		$this->validate = $validate;
-	}
-	
 	public function setConvert(Convert $convert) {
 		$this->convert = $convert;
+	}
+
+	public function hasConvert(): bool {
+		return $this->convert!==NULL;
 	}
 	
 	public function getConvert(): Convert {
 		return $this->convert;
 	}
 
+	public function setDefault(string $default) {
+		$this->default = $default;
+	}
+
+	public function hasDefault(): bool {
+		return $this->default!==NULL;
+	}
+	
 	public function getDefault(): string {
 		return $this->default;
 	}
 
-	public function getLongName(): string {
-		return $this->name;
-	}
-
-	public function getShortName(): string {
-		
-	}
-
-	public function getValidate(): Validate {
-		return $this->validate;
-	}
-
-	public function hasConvert(): bool {
-		return $this->convert!=NULL;
-	}
-
-	public function hasDefault(): bool {
-		return $this->default!=="";
+	public function setValidate(Validate $validate) {
+		$this->validate = $validate;
 	}
 
 	public function hasValidate(): bool {
 		return $this->validate!=NULL;
+	}
+
+	public function getValidate(): Validate {
+		return $this->validate;
 	}
 
 	public function isMandatory(): bool {
