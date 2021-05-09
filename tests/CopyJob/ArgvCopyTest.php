e2e3e48f0ab761cbdfbf5a82b1ceb68eaf73559b
DarthCorvidus <github@vm01.telton.de>
2021-05-09 21:12:05 +0200

    Forgot to test --from and --to.

--- tests/CopyJob/ArgvCopyTest.php
+++ tests/CopyJob/ArgvCopyTest.php
@@ -14,6 +14,8 @@ class ArgvCopyTest extends TestCase {
 		$array[] = __DIR__."/../target/";
 		$array[] = __DIR__."/../target.empty/";
 		$array[] = "--max=10";
+		$array[] = "--from=2010-01-01";
+		$array[] = "--to=2010-12-31";
 		$array[] = "--daily";
 		$array[] = "--weekly";
 		$array[] = "--monthly";
@@ -26,6 +28,8 @@ class ArgvCopyTest extends TestCase {
 		$this->assertEquals($array[1], $argv->getPositional(0));
 		$this->assertEquals($array[2], $argv->getPositional(1));
 		$this->assertEquals(10, $argv->getValue("max"));
+		$this->assertEquals("2010-01-01", $argv->getValue("from"));
+		$this->assertEquals("2010-12-31", $argv->getValue("to"));
 		$this->assertEquals(TRUE, $argv->getBoolean("daily"));
 		$this->assertEquals(TRUE, $argv->getBoolean("weekly"));
 		$this->assertEquals(TRUE, $argv->getBoolean("monthly"));
@@ -73,6 +77,31 @@ class ArgvCopyTest extends TestCase {
 		$array[] = "--max=15.3";
 		
 		$this->expectException(ArgvException::class);
+		$this->expectExceptionMessage("--max:");
+		$model = new ArgvCopy();
+		$argv = new Argv($array, $model);
+	}
+
+	function testFromBogus() {
+		$array[] = "lnkcopy.php";
+		$array[] = __DIR__."/../target/";
+		$array[] = __DIR__."/../target.empty/";
+		$array[] = "--from=Sometimes";
+		
+		$this->expectException(ArgvException::class);
+		$this->expectExceptionMessage("--from:");
+		$model = new ArgvCopy();
+		$argv = new Argv($array, $model);
+	}
+	
+	function testToBogus() {
+		$array[] = "lnkcopy.php";
+		$array[] = __DIR__."/../target/";
+		$array[] = __DIR__."/../target.empty/";
+		$array[] = "--to=Sometimes";
+		
+		$this->expectException(ArgvException::class);
+		$this->expectExceptionMessage("--to:");
 		$model = new ArgvCopy();
 		$argv = new Argv($array, $model);
 	}
